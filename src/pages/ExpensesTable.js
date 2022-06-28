eb056e97c73f2728f2d9ade68c72c7741d4b0d26
Eduardo <duduprado294@gmail.com>
2022-06-27 21:59:54 -0300

    Faz até o req 8

--- src/pages/ExpensesTable.js
+++ src/pages/ExpensesTable.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 class ExpensesTable extends React.Component {
   render() {
     const { expenses } = this.props;
+    console.log(expenses);
     return (
       <table>
         <tr>
@@ -18,6 +19,28 @@ class ExpensesTable extends React.Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        {expenses && expenses.map((item) => (
+          <tr key={ item.id }>
+            <td>{item.description}</td>
+            <td>{item.tag}</td>
+            <td>{item.method}</td>
+            <td>{Number(item.value).toFixed(2)}</td>
+            <td>
+              {/* {item.currency === 'USD' ? 'Dólar Comercial'
+                : item.currency === 'EUR' ? 'Euro' : item.currency} */}
+              {item.currency === 'USD' && 'Dólar Comercial'}
+              {item.currency === 'EUR' && 'Euro'}
+              {item.currency !== 'USD' && item.currency !== 'EUR' && item.currency}
+            </td>
+            <td>{Number(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+            <td>
+              {Number(item.value * item.exchangeRates[item.currency].ask)
+                .toFixed(2)}
+
+            </td>
+            <td>Real</td>
+          </tr>
+        ))}
       </table>
     );
   }
